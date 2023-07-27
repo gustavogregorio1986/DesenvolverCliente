4d641f2b3b29c673094c6a13cd43845ad474faf8
guga56 <gustavogregorio086@gmail.com>
2023-07-27 10:18:33 -0300

    inicializar o projeto , com layout

---
+++ ProjetoDesenvolvido/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoDesenvolvido.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
